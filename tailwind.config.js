8f748676838b1715378d83211c4f01288ecfbbf9
khademul mowla <khademulmowla31eee@gmail.com>
2024-08-25 08:17:55 +0600

    added largest portfolio,discuss section and footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
         extend: {
+            backgroundImage: {
+                'discuss-project': "url('/assets/bg.jpg')",
+            },
         },
     },
     plugins: [],
